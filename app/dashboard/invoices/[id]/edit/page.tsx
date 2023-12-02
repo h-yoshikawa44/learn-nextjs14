7062aa61d06a5f44a715832223305583d9d19a31
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-12-02 19:07:17 +0900

    ch12 - Updating an invoice - 2. Read the invoice id from page params

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,24 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { customers } from '@/app/lib/placeholder-data';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
